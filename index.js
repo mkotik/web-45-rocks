f508a3597ae91a2a5af46896deabd3eb896abaad
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-02 14:01:06 -0400

    created react

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 require("dotenv").config(); //this allows to stash "artificial" env variables in a file
+const cors = require("cors");
+const path = require("path");
 
 const server = express();
 
 const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
-
-server.get("/", (req, res) => {
-  res.send("<h1>web 45 ROCKS</h1>");
-});
+server.use(cors());
+server.use(express.static(path.join(__dirname, "client/build")));
 
 server.get("/api", (req, res) => {
   res.json({ message: "Web 45 is awesome!" });
+});
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 server.listen(PORT, () => {
